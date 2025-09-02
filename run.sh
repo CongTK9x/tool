7c2ac4d445c243dc42f8af9381632672836551d8
CongTK9x <155988811+CongTK9x@users.noreply.github.com>
2025-09-02 23:57:29 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,22 @@
+#!/bin/bash
 
+DOMAIN=$1      # domain:port
+HOSTNAME=$2    # hostname
+
+cat > config.json <<EOF
+{
+  "pools": [
+    {
+      "url": "$DOMAIN",
+      "user": "$HOSTNAME",
+      "pass": "x",
+      "tls": true,
+      "keepalive": true
+    }
+  ],
+  "cpu": {
+    "enabled": true,
+    "max-threads-hint": 80
+  }
+}
+EOF
